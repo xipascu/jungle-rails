534d35e6aeceb3dd701a23ca34df738bbb9ffb02
Ilinca Pascu <xipascu@gmail.com>
2017-11-11 07:42:50 +0000

    Email sending.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def receipt_email(order)
     @order = order
-    mail(to: @order.email, subject:"Here is your order receipt for #{order.id}")
+    mail(to: @order.email, subject:"Here is your order receipt for order ##{order.id}")
   end 
 
 end
